d5af8a9c7b247ca39acbc11334ec964a8f8a7abf
trembl <g@trembl.org>
2017-05-02 14:11:22 +0900

    Init file

---
+++ init-example.js
@@ -0,0 +1,15 @@
+// Initialize Firebase
+// 1. https://console.firebase.google.com/project/
+// 2. .. "Add Firebase to your project"
+// 3. Copy config data here
+// rename to init.js
+
+var config = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: ""
+};
+firebase.initializeApp(config);
